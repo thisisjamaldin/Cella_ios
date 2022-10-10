d791f956dda6614e7cc9bc0f387b62e3e3bd4537
thisisjamaldin <thisisjoma@gmail.com>
2022-10-10 17:10:15 +0600

    Initial Commit

---
+++ Cella/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cella
+//
+//  Created by Jamaldin on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
